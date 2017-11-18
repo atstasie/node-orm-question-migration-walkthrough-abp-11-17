3d2dd72e9669c2553671a1261b51350d86b8c6d1
Anastassia Troitskaia <atroitskaia@gmail.com>
2017-11-18 14:12:07 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -6,7 +6,7 @@ class Question{
   static CreateTable() {
     return new Promise(function(resolve) {
       const sql = `CREATE TABLE questions (
-        id INTEGER PRIMARY KEY
+        id INTEGER PRIMARY KEY,
         cntent TEXT
         )`
       db.run(sql, function(){
